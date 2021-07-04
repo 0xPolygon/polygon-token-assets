6ba175a859a4eb55adeae8b789be0033acb9db11
ujjwalgupta94 <ujjwal@matic.network>
2021-07-04 14:54:54 +0530

    add more icons

--- src/list.js
+++ src/list.js
@@ -7,10 +7,42 @@ const list = {
         symbol: "Kai",
         icon: "KAI.svg"
     },
+    aave: {
+        symbol: "Aave",
+        icon: "AAVE.svg"
+    },
+    ac: {
+        symbol: "Ac",
+        icon: "AC.svg"
+    },
+    adx: {
+        symbol: "Adx",
+        icon: "AGA.svg"
+    },
+    aga: {
+        symbol: "Aga",
+        icon: "AGA.svg"
+    },
     atri: {
         symbol: "Atri",
         icon: "ATRI.svg"
-    }
+    },
+    bid: {
+        symbol: "Bid",
+        icon: "BID.svg"
+    },
+    bs1: {
+        symbol: "Bs1",
+        icon: "BS1.svg"
+    },
+    "btc2x-fli": {
+        symbol: "BTC2x-FLI",
+        icon: "BTC2x-FLI.svg"
+    },
+    bzn: {
+        symbol: "BZN",
+        icon: "BZN.svg"
+    },
 }
 
 exports.list = list;
