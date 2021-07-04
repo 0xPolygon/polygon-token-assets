3bcd7179409cfd4d69b9631f3dcf7a70cec880eb
ujjwalgupta94 <ujjwal@matic.network>
2021-07-04 15:10:17 +0530

    implement search

--- docs/src/components/root.vue
+++ docs/src/components/root.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="root col content-center mt-20px">
     <h1 class="text-primary">Polygon Token Assets</h1>
+    <div>
+      <input
+        v-model="searchText"
+        placeholder="Search token"
+        type="text"
+        class="textbox mt-20px"
+        width="100px"
+        height="150px"
+      />
+    </div>
     <div class="row xs-12 mt-20px">
       <token-item v-for="token in tokens" :token="token" :key="token.icon" />
     </div>
@@ -15,11 +25,28 @@ export default {
   created() {
     this.tokens = tokens
   },
+  data() {
+    return {
+      searchText: '',
+    }
+  },
+  watch: {
+    searchText() {
+      const result = {}
+      const regex = new RegExp(this.searchText, 'i')
+      for (const key in tokens) {
+        const token = tokens[key]
+        if (token.symbol.match(regex)) {
+          result[key] = token
+        }
+      }
+      this.tokens = result
+    },
+  },
 }
 </script>
 
 <style scoped>
-.root{
-
+.root {
 }
 </style>
