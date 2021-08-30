68af897de524eca2b18a29abd3c49dc0f5d0aef3
ujjwalgupta94 <ujjwal@matic.network>
2021-08-30 15:04:10 +0530

    rm build:pack in install:lib

--- docs/install_lib.js
+++ docs/install_lib.js
@@ -4,7 +4,7 @@ const { execSync } = require('child_process');
 if (packageInfo) {
     const version = packageInfo.version;
     console.log('version', version);
-    execSync(`cd ../ && npm run build:pack`);
+   // execSync(`cd ../ && npm run build:pack`);
     execSync(`npm un ${packageInfo.name}`);
     execSync(`npm i ../${packageInfo.packageName}-${version}.tgz --no-save`)
 }
